bee325ad67cd0c5fb5e9bcb406f762e0051de4e9
svukelic <vukelic.stjepan@gmail.com>
2023-06-10 23:01:52 +0200

    Update script.js

--- static/script.js
+++ static/script.js
@@ -1,11 +1,23 @@
-function toggleCollapsible() {
-    console.log("jesam ga")
-  var collapsibleElement = document.getElementById("collapsible_week_description");
-  if (collapsibleElement.style.display === "none") {
-    collapsibleElement.style.display = "table-row";
-  } else {
-    collapsibleElement.style.display = "none";
-  }
-}
+document.addEventListener("DOMContentLoaded", function() {
+    function toggleCollapsible() {
+        var collapsible = document.getElementById("collapsible_week_description");
+        if (window.getComputedStyle(collapsible).display === "none") {
+            collapsible.style.display = "table-row";
+        } else {
+            collapsible.style.display = "none";
+        }
+    }
 
-document.getElementById("collapsable_week_description_toggle_td").addEventListener("click", toggleCollapsible);
+    function toggleCalculations() {
+        var collapsible = document.getElementById("collapsable-pace-calculations");
+        if (window.getComputedStyle(collapsible).display === "none") {
+            collapsible.style.display = "block";
+        } else {
+            collapsible.style.display = "none";
+        }
+    }
+
+    document.getElementById("person_week_in_training_td").addEventListener("click", toggleCollapsible);
+    document.getElementById("calculator-icon").addEventListener("click", toggleCalculations);
+
+});
